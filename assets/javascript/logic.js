83631ea6ce1ccb253f642d80f4e289460d7f3b01
Terry <trrychng@gmail.com>
2017-12-15 23:44:38 -0500

    update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,21 +5,18 @@
 var animals = ["bird", "cat", "dog"];
 
       function renderButtons() {
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
+
         $("#buttons-view").empty();
-        // Looping through the array of movies
+
         for (var i = 0; i < animals.length; i++) {
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie to our button
+        var a = $("<button>");
+
           a.addClass("animal");
-          // Adding a data-attribute
+
           a.attr("data-name", animals[i]);
-          // Providing the initial button text
+
           a.text(animals[i]);
-          // Adding the button to the buttons-view div
+
           $("#buttons-view").append(a);
         }
       }
@@ -28,21 +25,21 @@ var animals = ["bird", "cat", "dog"];
 
 
 
-      // This function handles events where a movie button is clicked
+
       $("#add-animal").on("click", function() {
         event.preventDefault();
-        // This line grabs the input from the textbox
+
         var animal = $("#animal-input").val().trim();
 
         console.log(animal);
-        // Adding movie from the textbox to our array
+
         animals.push(animal);
-        // Calling renderButtons which handles the processing of our movie array
+
         renderButtons();
 
       });
 
-renderButtons()
+
 
 
 
@@ -107,3 +104,6 @@ renderButtons()
       }
 
     });
+
+
+renderButtons()
